ba88bf8b873ed165f5162a8b3cf3d2d5705bfc2a
Andrew Kozlik <akozlik@gmail.com>
2011-04-11 21:22:53 -0400

    DB Changes

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$username = "dbo280008450";
-$password = "B54Y2Mu9";
-$host = "db791.perfora.net";
-$database = "db280008450";
+$username = "";
+$password = "";
+$host = "";
+$database = "";
 
 $conn = mysql_connect($host, $username, $password);
 $db = mysql_select_db($database, $conn);
